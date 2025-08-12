a20d6cb3c683f8db7418ebff551aa5950e320999
HPC-dev-acc <nights.hustle3a@icloud.com>
2025-08-12 23:43:38 +0900

    Fix slide width on iPad Safari

--- src/game/width.js
+++ src/game/width.js
@@ -1,7 +1,9 @@
 export const BASE_W = 84;
 
 export function updatePlayerWidth(player) {
-  if (!player.running && !player.blocked && player.onGround) {
+  if (player.sliding > 0) {
+    player.w = BASE_W;
+  } else if (!player.running && !player.blocked && player.onGround) {
     player.w = BASE_W * 2 / 3;
   } else {
     player.w = BASE_W;
